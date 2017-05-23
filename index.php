e22d07798b40472a32fdb19d705a4bb550f1f9fc
Rémy <remdub@live.fr>
2017-05-23 16:40:02 +0200

    finish the loop that displays products

--- index.php
+++ index.php
@@ -3,16 +3,11 @@
 <main>
     <h2>Spring Summer 2017</h2>
     <section>
-        <article>
-            <a href="file:///home/remy/Code/intShopSass/product.html"><img src="http://placehold.it/175.5x200" alt="image produit" class="img-responsive"></a>
-            <h4 class="buy">buy</h4>
-            <p>Random product Name</p>
-            <span>€200.00</span>
-        </article>
 
         <?php
         $args = array(
-            'category' => 3
+            'category' => 3,
+            'order' => "ASC"
         );
         $posts = get_posts($args);
         foreach ($posts as $post) { ?>
@@ -21,18 +16,30 @@
                 <a href="file:///home/remy/Code/intShopSass/product.html"><img src="http://placehold.it/175.5x200" alt="imageproduit" class="img-responsive"></a>
 
                 <?php  $cats = get_the_category($post->ID);
-                        foreach ($cats as $cat) {
-                            if ($cat->cat_ID == 4) {?>
+                $hasSubClass = false;
+                foreach ($cats as $cat) {
+                    if ($cat->cat_ID == 4) { ?>
+
+                        <h4 class="<?php echo $cat->slug ?>"><?php echo $cat->name ?></h4>
+
+                <?php } elseif ($cat->cat_ID == 5) { ?>
+
+                            <h4 class="<?php echo $cat->slug ?>"><?php echo $cat->name ?></h4>
+
+                <?php } elseif ($cat->cat_ID == 6) { ?>
 
                                 <h4 class="<?php echo $cat->slug ?>"><?php echo $cat->name ?></h4>
-                        <?php }
-                        } ?>
 
-            </article>
+                <?php }
+                }
+                $meta = get_post_meta($post->ID, "Price"); ?>
+                            <p><?php echo $post->post_title ?></p>
+                            <span><?php echo $meta[0] ?></span>
+                        </article>
 
 
-        <?php } ?>
-    </section>
-</main>
+                        <?php } ?>
+                    </section>
+                </main>
 
-<?php get_footer();
+                <?php get_footer();
